095e68bd951a17efce216dfb315ff2da1315ea9b
Surajit <surajitjana20107@gmail.com>
2026-04-14 11:40:21 +0000

    update calculator bash script

--- calc.sh
+++ calc.sh
@@ -8,33 +8,26 @@ echo "e for get remainder"
 
 read -p "Your choice?: " choice
 
+read -p "First value: " num1
+read -p "Second value: " num2
+
 function addition {
-	read -p "First value: " num1
-	read -p "Second value: " num2
 	echo "$num1 + $num2 = $(($num1 + $num2))"
 }
 
 function subtraction {
-	read -p "First value: " num1
-	read -p "Second value: " num2
 	echo "$num1 - $num2 = $(($num1 - $num2))"
 }
 
 function multiplication {
-	read -p "First value: " num1
-	read -p "Second value: " num2
 	echo "$num1 x $num2 = $(($num1 * $num2))"
 }
 
 function division {
-	read -p "First value: " num1
-	read -p "Second value: " num2
 	echo "$num1 / $num2 = $(($num1 / $num2))"
 }
 
 function remainder {
-	read -p "First value: " num1
-	read -p "Second value: " num2
 	echo "$num1 % $num2 = $(($num1 % $num2))"
 }
 
