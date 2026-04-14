919128d25abb45c381df4650f5ccf66550c280f6
Surajit <surajitjana20107@gmail.com>
2026-04-14 12:45:44 +0000

    fix everytime success status

--- 27_exit-status.sh
+++ 27_exit-status.sh
@@ -3,10 +3,11 @@
 # $? it's an special variable if its retuen 0 means previous command was success and other numbers means failed
 read -p "Enter url: " site
 curl -i $site
-sleep 5s
 
 if [[ $? -eq 0 ]]; then
+	echo
 	echo "Ping Success...!!"
 else
+	echo
 	echo "Ping Failed...!!"
 fi
